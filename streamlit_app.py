b2641426615724bcb455336aa66d1474c386475e
Phypher <phypher@gmail.com>
2022-06-22 21:02:06 +0100

    argghghghghghghg

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
-mport requests
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
