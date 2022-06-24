743b47767ca1d7baf38add9595f99e015d42bf9d
Phypher <phypher@gmail.com>
2022-06-24 21:12:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ def get_fruityvice_data(this_fruit_choice):
 streamlit.header("Fruityvice Fruit Advice!")
 
 try:
-    fruit_choice = streamlit.text_input('What fruit would you like informationa about?')
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:
@@ -42,15 +42,28 @@ streamlit.write('The user entered ', fruit_choice)
 
 streamlit.stop()
 
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+#Snowflake related functions
+def get_fruit_load_list():
+        with my_cnx.cursor() as my_cur:
+                my_cur.execute("select * from fruit_load_list")
+                return my_cur.fetchall()
+
+        #Add a button to laod the fruit
+        if streamlit.button('Get fruit load list')
+                my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+                my_data_rows = get_fruit_load_list()
+                streamlit.dataframe(my_data_rows)                
+
+
+
+
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
+#streamlit.header("The fruit load list contains:")
+#streamlit.dataframe(my_data_rows)
+#add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+#streamlit.write('Thanks for adding ', add_my_fruit)
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')");
